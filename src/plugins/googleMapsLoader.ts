e83c3a2d7588b066eac35d2df5bcabcdd8f85713
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2026-06-05 00:58:06 +0800

    Removing places

--- src/plugins/googleMapsLoader.ts
+++ src/plugins/googleMapsLoader.ts
@@ -4,7 +4,7 @@ import { Loader } from '@googlemaps/js-api-loader'
 const loader = new Loader({
     apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
     version: 'weekly',
-    libraries: ['maps', 'marker', 'places'],
+    libraries: ['maps', 'marker'],
     id: '__googleMapsScriptId', // ensure consistent script id
 })
 
